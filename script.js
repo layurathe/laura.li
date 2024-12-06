f7cbf3849ab0246f1a011afa75c41935dcd33594
layurathe <laura.li_li@outlook.com>
2024-12-05 21:50:57 -0500

    Thesis File setup

--- script.js
+++ script.js
@@ -21,7 +21,6 @@ function showSlides(n) {
     slides[i].style.display = "none";
   }
   slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
   captionText.innerHTML = dots[slideIndex-1].alt;
 }
 
